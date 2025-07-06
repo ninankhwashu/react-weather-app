f5c7af1cc50a148e1ab571aebecb4d794dc2c04d
ninankhwashu <ninankhwashu@gmail.com>
2025-07-06 23:41:33 +0200

    Add footer for credit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://linkedin.com/in/ninankhwashu"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nina Nkhwashu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ninankhwashu/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
